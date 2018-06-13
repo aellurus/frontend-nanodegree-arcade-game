234fe416194d56a7e95fb0060c8cbd622df3373c
Dora Aunedi <aellurus@gmail.com>
2018-06-13 15:21:38 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,85 @@
+var allEnemies = [];// Enemies our player must avoid
+//DZ - stavi enemya na mapu!!
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
+
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+var Enemy = function(horizontalPosition, verticalPosition) {
+    this.offset=25;
+    this.x = 101*horizontalPosition;
+    this.y = 83*verticalPosition-this.offset;
+    this.sprite = 'images/enemy-bug.png';
+};
+
+allEnemies.push(new Enemy(0,1));
+allEnemies.push(new Enemy(1,2));
+allEnemies.push(new Enemy(2,3));
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+
+//konstruktor
+var Player = function(horizontalPosition, verticalPosition) {
+    this.offset=40;
+    this.x = 101*horizontalPosition;
+    this.y = 83*verticalPosition - this.offset;
+    this.sprite = 'images/char-boy.png';
+};
+
+var player = new Player(2,5);
+
+Player.prototype.update = function(dt) {
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function (key) {
+  if (key==='up'&& this.y>0) {
+    this.y = this.y-83;
+    //this.y -= 83; ako je netko to napisao ošamariš ga
+  }
+  if (key==='down'&& this.y<5*83-this.offset) {
+    this.y = this.y+83;
+  }
+  if (key==='left'&& this.x>0) {
+    this.x = this.x-101;
+  }
+  if (key==='right'&& this.x<4*101) {
+    this.x = this.x+101;
+  }
+};
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
