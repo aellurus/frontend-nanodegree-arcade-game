514c4e9cb9bcf6ae9218df7c2b15ce8fe2159f0a
Dora Aunedi <aellurus@gmail.com>
2018-06-13 22:31:53 +0200

    Hit detection

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-var allEnemies = [];// Enemies our player must avoid
+var allEnemies = [];
+var blockWidth = 101;
+var blockHeight = 83;
+// Enemies our player must avoid
 //DZ - stavi enemya na mapu!!
   // Variables applied to each of our instances go here,
   // we've provided one for you to get started
@@ -6,11 +9,11 @@ var allEnemies = [];// Enemies our player must avoid
   // The image/sprite for our enemies, this uses
   // a helper we've provided to easily load images
 var Enemy = function(horizontalPosition, verticalPosition, speed) {
-    this.speed=speed;
-    this.offset=25;
-    this.horizontalPosition=horizontalPosition;
-    this.x = 101*horizontalPosition;
-    this.y = 83*verticalPosition-this.offset;
+    this.speed = speed;
+    this.offset = 25;
+    this.horizontalPosition = horizontalPosition;
+    this.x = blockWidth * horizontalPosition;
+    this.y = blockHeight * verticalPosition-this.offset;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -22,11 +25,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    console.log(ctx.canvas.width);
-    if (this.x<ctx.canvas.width){
-     this.x=this.x+this.speed;
+    if (this.x < ctx.canvas.width){
+     this.x = this.x + this.speed;
     } else {
-        this.x=101*this.horizontalPosition;
+        this.x = blockWidth * this.horizontalPosition;
     }
    
 };
@@ -42,15 +44,29 @@ Enemy.prototype.render = function() {
 
 //konstruktor
 var Player = function(horizontalPosition, verticalPosition) {
-    this.offset=40;
-    this.x = 101*horizontalPosition;
-    this.y = 83*verticalPosition - this.offset;
+    this.offset = 40;
+    this.horizontalPosition = horizontalPosition;
+    this.verticalPosition = verticalPosition;
+    this.x = blockWidth * horizontalPosition;
+    this.y = blockHeight * verticalPosition - this.offset;
     this.sprite = 'images/char-boy.png';
+    this.threshold = 5;
 };
 
 
+Player.prototype.update = function() {
+    var min = this.x - this.threshold;
+    var max = this.x + this.threshold;
+    var self = this;
+    allEnemies.forEach(function (enemy){
+        if (enemy.x > min && enemy.x < max && enemy.y+enemy.offset===self.y+self.offset){
+            self.x = blockWidth * self.horizontalPosition;
+            self.y = blockHeight * self.verticalPosition - self.offset;
+        }
+    })
 
-Player.prototype.update = function(dt) {
+        
+    
 };
 
 Player.prototype.render = function() {
@@ -58,18 +74,18 @@ Player.prototype.render = function() {
 };
 
 Player.prototype.handleInput = function (key) {
-  if (key==='up'&& this.y>0) {
-    this.y = this.y-83;
-    //this.y -= 83; ako je netko to napisao ošamariš ga
+  if (key==='up' && this.y > 0) {
+    this.y = this.y-blockHeight;
+    //this.y -= blockHeight; ako je netko to napisao ošamariš ga
   }
-  if (key==='down'&& this.y<5*83-this.offset) {
-    this.y = this.y+83;
+  if (key==='down' && this.y < 5 * blockHeight - this.offset) {
+    this.y = this.y+blockHeight;
   }
-  if (key==='left'&& this.x>0) {
-    this.x = this.x-101;
+  if (key==='left' && this.x > 0) {
+    this.x = this.x-blockWidth;
   }
-  if (key==='right'&& this.x<4*101) {
-    this.x = this.x+101;
+  if (key==='right' && this.x < 4 * blockWidth) {
+    this.x = this.x + blockWidth;
   }
 };
 // Now instantiate your objects.
